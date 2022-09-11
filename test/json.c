06bee2127228e9346d604ed19ef74fb068db4dbd
sleepntsheep <contact@papangkorn.com>
2022-09-11 12:27:02 +0700

    json.h - fix bugs

--- test/json.c
+++ test/json.c
@@ -5,15 +5,29 @@
 #include <stdio.h>
 
 
+const char *tests[]=  {
+    "{ \"key\": \"value\", \"num\": -2839.489 } ",
+    "{\"obj\": {}}", 
+    "[1, 2]",
+    "[]",
+    "[[]]",
+    "{}",
+    "[[{}, {}]]",
+    "{\"a\": [1,2]}", 
+    "[{\"b\": [1,2]}]", 
+};
+
 int main() {
     sjson_register_logger(printf);
-
-    sjsontokarr *toks;
-    toks = sjson_lex(strdup("{ \"key\": \"value\", \"num\": -2839.489 } "));
-    sjson_print_tokarr(toks);
-    sjson *json;
-    json = sjson_parse(toks);
-    sjson_debug_print(json, 0);
+    for (int i =0  ;i < sizeof tests / sizeof *tests; i++) {
+        puts(tests[i]);
+        sjsontokarr *toks;
+        toks = sjson_lex(strdup(tests[i]));
+        sjson_print_tokarr(toks);
+        sjson *json;
+        json = sjson_parse(toks);
+        sjson_debug_print(json, 0);
+    }
 
     //json = sjson_parse(strdup("{ \"nums\": 12, \"n2\": 390.2, \"arr\": [ 2, 3, 4, \"POG\" ] }"));
     //sjson_debug_print(json.sjson,0);
