6390b5cf0356deed4823b917c609015adb288cba
sleepntsheep <contact@papangkorn.com>
2022-09-26 20:57:49 +0700

    remove docs folder

--- docs/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5fdynarray_5finfo_0',['_dynarray_info',['../struct__dynarray__info.html',1,'']]]
-];
--- docs/search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['prev_0',['prev',['../structsjson.html#a2bdc8349e7639095ad2bc0f5477cd65d',1,'sjson']]]
-];
