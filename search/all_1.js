9499f1716cbf556baf9ddd8ba84935f3cfb3ba50
sleepntsheep <83170424+sleepntsheep@users.noreply.github.com>
2022-09-26 13:52:48 +0000

    Deploying to gh-pages from  @ 6412e228d4af3f5c98466e3cb7bdbcde371cabc2 🚀

---
+++ search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buf_0',['buf',['../structsjsonbuf.html#a2fb48c8edb2a0b14c090d67d12979e71',1,'sjsonbuf']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "_bcdiklnpstvw",
+  1: "_s",
+  2: "dls",
+  3: "s",
+  4: "bcknpstv",
+  5: "dipsw",
+  6: "s"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "defines",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Macros",
+  6: "Pages"
+};
+
