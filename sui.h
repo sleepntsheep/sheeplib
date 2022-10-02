ca126222b22b9ce0740c9ba149a20ddc43c48159
sleepntsheep <contact@papangkorn.com>
2022-10-02 23:07:04 +0700

    e

--- sui.h
+++ sui.h
@@ -1,3 +1,8 @@
+/* @file sui.h
+ * @brief intermediate ui library
+ *
+ *
+ */
 #ifndef SUI_H_
 #define SUI_H_
 
@@ -28,12 +33,17 @@ enum mouse_button {
     MOUSE_BUTTON_COUNT,
 };
 
-typedef struct {
+typedef struct sui_ctx_s sui_ctx;
+
+struct sui_ctx_s {
     int selected_id;
     int mouse_pressed; /* mouse_button OR'd together */
     int mouse_x, mouse_y;
     sui_style style;
-} sui_ctx;
+    void (*draw_rect)(sui_ctx *, int, int, int, int);
+    void (*draw_text)(sui_ctx *, const char *text, int, int);
+    void (*get_text_size)(const char *text, int *w, int *h);
+};
 
 static const sui_style sui_style_default = {
     .bg = {0xff, 0xff, 0xff, 0xff},
@@ -43,13 +53,8 @@ static const sui_style sui_style_default = {
 
 void sui_ctx_init(sui_ctx *ctx);
 bool sui_btn(sui_ctx *ctx, const char *label, int x, int y, int id);
+void sui_text(sui_ctx *ctx, const char *label, int x, int y);
 inline bool sui_intersect(int x, int y, int w, int h, int mx, int my);
-
-/* these function are left for backend to implement */
-void sui_draw_rect(sui_ctx *ctx, int x, int y, int w, int h);
-void sui_draw_text(sui_ctx *ctx, const char *text, int x, int y);
-/* font is managed by backend, so to draw centered text we need this */
-void sui_get_text_size(const char *text, int *w, int *h);
 
 #endif /* SUI_H_ */
 
@@ -67,13 +72,13 @@ void sui_ctx_init(sui_ctx *ctx) {
 
 bool sui_btn(sui_ctx *ctx, const char *label, int x, int y, int id) {
     int w, h;
-    sui_get_text_size(label, &w, &h);
+    ctx->get_text_size(label, &w, &h);
     w += 2 * ctx->style.btn_pad;
     h += 2 * ctx->style.btn_pad;
     int tx = x + ctx->style.btn_pad;
     int ty = y + ctx->style.btn_pad;
-    sui_draw_rect(ctx, x, y, w, h);
-    sui_draw_text(ctx, label, tx, ty);
+    ctx->draw_rect(ctx, x, y, w, h);
+    ctx->draw_text(ctx, label, tx, ty);
     if (!ctx->mouse_pressed) {
         ctx->selected_id = -1;
         return false;
@@ -84,12 +89,22 @@ bool sui_btn(sui_ctx *ctx, const char *label, int x, int y, int id) {
         return false;
     if (sui_intersect(x, y, w, h, ctx->mouse_x, ctx->mouse_y)) {
         ctx->selected_id = id;
-        return true; 
-    }
-    else {
+        return true;
+    } else {
         return false;
     }
 }
 
-#endif /* SUI_IMPLEMENTATION */
+void sui_text(sui_ctx *ctx, const char *label, int x, int y) {
+    int w, h;
+    ctx->get_text_size(label, &w, &h);
+    w += 2 * ctx->style.btn_pad;
+    h += 2 * ctx->style.btn_pad;
+    int tx = x + ctx->style.btn_pad;
+    int ty = y + ctx->style.btn_pad;
+    ctx->draw_rect(ctx, x, y, w, h);
+    ctx->draw_text(ctx, label, tx, ty);
+}
 
+#undef SUI_IMPLEMENTATION
+#endif /* SUI_IMPLEMENTATION */
